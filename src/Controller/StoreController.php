584374f84af6305219a85154a8bae83ff32a68e5
TRoll_94 <surnin2003@mail.ru>
2023-06-15 10:53:02 +0200

    Added list products to main screen

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\CategorySelectType;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,14 +18,29 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('PUBLIC_ACCESS')]
 class StoreController extends AbstractController
 {
+
     #[Route('/', name: 'app_store_index')]
-    public function num(Request $request, UserRepository $userRepository): Response
+    public function num(Request $request, UserRepository $userRepository, CategoryRepository $categoryRepository,
+                        ProductRepository $productRepository,
+                        LoggerInterface $logger
+    ): Response
     {
+        $category_selected = $request->get('category_select')['category'] ?? '';
+        $logger->error("=====================");
+        $logger->error(print_r($category_selected, true));
+        $products = $productRepository->findBy(['category' => $category_selected]);
+
+        $category_select = $categoryRepository->findAll();
+        $category_select_form = $this->createForm(CategorySelectType::class, $category_select, options: ['data_class'=>null]);
+        $category_select_form->handleRequest($request);
+
         $user = $this->getUser();
         $is_anon = !$user instanceof UserInterface;
 
         return $this->render('store/main.html.twig', [
             'is_anon' => $is_anon,
+            'category_select_form' => $category_select_form,
+            'products' => $products,
         ]);
     }
 }
---
+++ src/Form/CategorySelectType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategorySelectType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('category', EntityType::class, [
+                'choice_value' => 'id',
+                'choice_label' => 'name',
+                'data' => '',
+                'class' => Category::class,
+                'label' => 'Category',
+                'multiple' => false,
+                'required' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,function ($category) {
+                    return $category->getName();
+                }
+        ]);
+    }
+}
